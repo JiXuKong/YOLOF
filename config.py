3b914e9cc9dceaa413bdd2cb37cfb98d3d5517f6
JiXuKong <45458826+JiXuKong@users.noreply.github.com>
2021-11-28 20:57:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ ssl_label_path = ''
 
 cache_path = './pkl'
 
-val_restore_path = './checkpoint/model.ckpt-25049'
-# train_restore_path = './checkpoint/model.ckpt-12000'
+val_restore_path = './checkpoint/model.ckpt-35000'
+# train_restore_path = './checkpoint/model.ckpt-30000'
 train_restore_path = './pretrained/resnet_v1_50_2016_08_28/resnet_v1_50.ckpt'
 
 #data parameter
@@ -42,21 +42,21 @@ class_to_ind = 0
 #some network parameters
 weight_decay = 0.0001
 gradient_clip_by_norm = 10
-batch_size = 1#测试时改为1
+batch_size = 6#测试时改为1
 max_epoch = 1
 with open(test_img_txt, 'r') as f:
     image_index = [x.strip() for x in f.readlines()]
 test_num = len(image_index)
 # test_num = 341//batch_size
 class_num = len(classes)
-image_size = 800
+image_size = 640#currently only
 feature_size = [[image_size//(2**i), image_size//(2**i)] for i in range(3,8)]
 phase = True
 # base_anchor = [32, 64, 128]
 base_anchor = [16]
 scale = np.array([2, 4, 8, 16, 32])
 # scale = np.array([1])
-aspect_ratio = np.array([1.0])
+aspect_ratio = np.array([1.0])#np.array([0.5, 1.0, 2.0])
 anchors = scale.shape[0]*aspect_ratio.shape[0]
 momentum_rate = 0.9
 alpha = 0.25
@@ -67,10 +67,10 @@ decay = 0.99
 pi = 1e-2
 
 #yolof 
-LR = 0.12/64*batch_size/10#0.01/(16/batch_size)
-DECAY_STEP = [train_num//batch_size*20, train_num//batch_size*27]
-score_threshold=0.05
-nms_iou_threshold=0.6
+LR = 1e-3#0.12/64*batch_size/7#0.01/(16/batch_size)
+DECAY_STEP = [train_num//batch_size*40, train_num//batch_size*50]
+score_threshold=0.005
+nms_iou_threshold=0.5
 encoder_channels=512
 block_mid_channels=128
 num_residual_blocks=4
@@ -78,5 +78,5 @@ block_dilations=[2, 4, 6, 8]
 cls_num_convs=2
 reg_num_convs=4
 match_times=4
-max_detection_boxes_num=50
+max_detection_boxes_num=100
 giou_loss = False
